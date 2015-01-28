417f2a5571bdafcf71bce6a0462bf307d03ee117
Andrew Kelley <superjoe30@gmail.com>
2015-01-28 15:44:17 -0700

    add more API

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
 #![allow(unstable)]
 
+use std::ops::Index;
+
 #[derive(Copy, Clone, PartialEq)]
 pub struct Matrix4([[f32; 4]; 4]);
+
+impl Index<usize> for Matrix4 {
+    type Output = [f32; 4];
+
+    fn index<'a>(&'a self, row: &usize) -> &'a [f32; 4] {
+        &self.0[*row]
+    }
+}
 
 impl Matrix4 {
     pub fn as_array(&self) -> &[[f32; 4]; 4] {
@@ -10,6 +20,39 @@ impl Matrix4 {
 
     pub fn from_array(array: [[f32; 4]; 4]) -> Self {
         Matrix4(array)
+    }
+
+    pub fn identity() -> Self {
+        Matrix4::from_array([[1.0, 0.0, 0.0, 0.0],
+                          [0.0, 1.0, 0.0, 0.0],
+                          [0.0, 0.0, 1.0, 0.0],
+                          [0.0, 0.0, 0.0, 1.0]])
+    }
+
+    pub fn operate<F: Fn(f32) -> f32>(&mut self, f: F) {
+        self.0[0][0] = f(self.0[0][0]);
+        self.0[0][1] = f(self.0[0][1]);
+        self.0[0][2] = f(self.0[0][2]);
+        self.0[0][3] = f(self.0[0][3]);
+
+        self.0[1][0] = f(self.0[1][0]);
+        self.0[1][1] = f(self.0[1][1]);
+        self.0[1][2] = f(self.0[1][2]);
+        self.0[1][3] = f(self.0[1][3]);
+
+        self.0[2][0] = f(self.0[2][0]);
+        self.0[2][1] = f(self.0[2][1]);
+        self.0[2][2] = f(self.0[2][2]);
+        self.0[2][3] = f(self.0[2][3]);
+
+        self.0[3][0] = f(self.0[3][0]);
+        self.0[3][1] = f(self.0[3][1]);
+        self.0[3][2] = f(self.0[3][2]);
+        self.0[3][3] = f(self.0[3][3]);
+    }
+
+    pub fn scale(&mut self, scalar: f32) {
+        self.operate( |x| { x * scalar } )
     }
 }
 
@@ -22,7 +65,7 @@ pub struct Vector4 {
 }
 
 #[test]
-fn matrix_to_array() {
+fn test_matrix_to_array() {
     let array = {
         let foo = Matrix4::from_array([[1.0, 0.0, 0.0, 0.0],
                                        [0.0, 1.0, 0.0, 0.0],
@@ -32,4 +75,12 @@ fn matrix_to_array() {
         *foo.as_array()
     };
     assert_eq!(array[1][1], 1.0);
+}
+
+#[test]
+fn test_scale() {
+    let mut m = Matrix4::identity();
+    assert_eq!(m[2][2], 1.0);
+    m.scale(3.0);
+    assert_eq!(m[2][2], 3.0);
 }
